51adc39a6c97c142cadc1d95bf9eee1c4bd73c3c
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-11-30 17:33:51 +0100

    style update

--- index.php
+++ index.php
@@ -5,27 +5,22 @@ $navbarMenu= [
     [
         "text"=>"Introduzione",
         "link"=>"#",
-        "active"=>false,
     ],
     [
         "text"=>"Norme sulla privacy",
         "link"=>"#",
-        "active"=>false,
     ],
     [
         "text"=>"Termini di servizio",
         "link"=>"#",
-        "active"=>false,
     ],
     [
         "text"=>"Tecnologie",
         "link"=>"#",
-        "active"=>false,
     ],
     [
         "text"=>"Domande frequenti",
         "link"=>"#",
-        "active"=>true,
     ],
 ];
 $faq= 
@@ -34,7 +29,7 @@ $faq=
         "question"=> "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
         "answer"=> 
         [
-            "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. 
+            "La recente <a href='#'>decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. 
             La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati 
             relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere 
             inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
@@ -43,7 +38,7 @@ $faq=
             Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto 
             dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.",
             
-            "Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione 
+            "Per presentare una richiesta di rimozione, compila questo <a href='#'>modulo web</a>. Riceverai una risposta automatica che conferma la ricezione 
             della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto 
             tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata.
             Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se 
@@ -75,9 +70,9 @@ $faq=
             sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi 
             e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.",
             
-            "Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.",
+            "Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href='#'>Centro Google per la sicurezza online.</a>",
 
-            "Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
+            "<a href='#'>Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
 
             ]
     ],
@@ -113,7 +108,7 @@ $faq=
 
             "Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui 
             lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza 
-            vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.",
+            vicino a un confine territoriale. <a href='#'>Contattaci</a> se ritieni che il paese associato al tuo account sia sbagliato.",
 
         ]
     ],
@@ -124,13 +119,13 @@ $faq=
             "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. 
             I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere 
             risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere
-            qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli
+            qualcosa dal Web, devi <a href='#'>contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli
             di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, 
             desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati 
-            di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google 
+            di ricerca di Google, <a href='#'>fai clic qui</a>. Una volta che i contenuti saranno stati rimossi e che Google 
             avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca
-            di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra 
-            pagina di assistenza per avere ulteriori informazioni.",
+            di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href='#'>visitare la nostra 
+            pagina di assistenza per avere ulteriori informazioni.</a>",
 
         ]
     ],
@@ -140,12 +135,12 @@ $faq=
         [
             "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web
             potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, 
-            della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della 
+            della pagina dei risultati di ricerca sotto forma di <a href='#'>URL referrer</a>. Talvolta, l'URL della 
             pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. 
             Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte 
             dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. 
             Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi.
-            Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le 
+            Ulteriori informazioni sulla ricerca SSL sono disponibili <a href='#'>qui</a>. Le query di ricerca o le 
             informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google 
             Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero
             ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.",
@@ -208,7 +203,7 @@ $faq=
                 foreach ($navbarMenu as $voce) {
                     $text= $voce["text"];
                     $link= $voce["link"];
-                    $active= $voce["active"];
+                    
                 ?>
 
                     <li><a href="<?php echo $link ?>"><?php echo $text ?></a></li>
@@ -230,7 +225,7 @@ $faq=
                     $answer= $value["answer"];
                     /* echo $question; */
                 ?>
-                <h2 class="mt-5"><?php echo $question ?></h2>
+                <h4 class="pt-5"><?php echo $question ?></h4>
                     <ul class="mt-3">
 
                         <!-- ciclo figlio che stampa i singoli paragrafi all'interno di <li> -->
