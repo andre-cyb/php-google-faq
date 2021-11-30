e58e8c05deb71356934f07910fdebfbdd0ed067d
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-11-30 18:39:20 +0100

    esercizio completato

--- index.php
+++ index.php
@@ -80,17 +80,20 @@ $faq=
         "question"=> "Perché il mio account è associato a un paese?",
         "answer"=> 
         [
-            "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
+            
+            "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:<ol>",
+            
 
-            "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto 
+            "<ol type='a'>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto 
             delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle 
             due società seguenti:",
+            
 
             "Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, 
             oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.",
-            "Google LLC, con sede negli Stati Uniti, per il resto del mondo.",
+            "Google LLC, con sede negli Stati Uniti, per il resto del mondo.</ol>",
 
-            "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.",
+            "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali. </ol>",
 
             "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.",
 
@@ -245,6 +248,8 @@ $faq=
                 <!--  -->  
         </div>
     </main>
-    <footer></footer>
+    <footer>
+        
+    </footer>
 </body>
 </html>
